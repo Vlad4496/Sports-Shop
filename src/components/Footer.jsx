0113e32a55363996a0ebf64f7df8f66ee315800f
Vlad4496 <vlad_4496@yahoo.com>
2020-08-02 10:54:39 +0300

    message

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,31 +20,31 @@ function Footer() {
                 <div className="footer-group">
                     <h3 className="h5">Contactează-mă:</h3>
                     <p className="m-0">
-                        <a href="mailto:razvan.cirlugea@gmail.com" className="text-dark">
+                        <a href="mailto:vlad_4496@yahoo.com" className="text-dark">
                             <Mail className="mr-1 mb-1 footer-icon"/>
-                            razvan.cirlugea@gmail.com
+                            vlad_4496@yahoo.com
                         </a>
                     </p>
-                    <p className="m-0"><Phone className="mr-1 footer-icon"/>+40728998166</p>
+                    <p className="m-0"><Phone className="mr-1 footer-icon"/>+40799335599</p>
                 </div>
                 <div className="footer-group">
                     <h3 className="h5">Contactează-mă:</h3>
                     <p className="m-0">
-                        <a href="https://github.com/razvancir96" className="text-dark">
+                        <a href="https://github.com/Vlad4496" className="text-dark">
                             <GitHub className="mr-1 mb-1 footer-icon"/>
-                            razvancir96
+                            Vlad4496
                         </a>
                     </p>
                     <p className="m-0">
-                        <a href="https://www.linkedin.com/in/razvancirlugea/" className="text-dark">
+                        <a href="https://www.linkedin.com/in/vlad-raul-merisor-5b8b67195/" className="text-dark">
                             <LinkedIn className="mr-1 footer-icon"/>
-                            razvancirlugea
+                            vladraulmerisor
                         </a>
                     </p>
                 </div>
             </div>
             <div className="text-center py-3">
-                &copy; Răzvan Cîrlugea, 2019
+                &copy; Vlad Raul Merisor, 2020
             </div>
         </footer>
     );
